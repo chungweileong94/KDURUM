5e5139c0e10078d7b133bbbc21c401e54e7150a2
Chung Wei Leong <leongchungwei@hotmail.com>
2018-02-13 20:42:16 +0800

    added delete account  dialog

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,16 +1,47 @@
 <template>
-  <div class="container-fluid">
-      <div class="row">
-          <div class="col-sm-12">
-              <img class="img-circle center-block" style="width:100px;" v-bind:src="user.thumnail">
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-sm-12">
-              <h4 class="text-center"><b>{{ user.name }}</b></h4>
-          </div>
-      </div>
-  </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <img class="img-circle center-block" style="width:100px;" v-bind:src="user.thumnail">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h4 class="text-center">
+                    <b>{{ user.name }}</b>
+                </h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h5 class="text-center">{{ user.role }}</h5>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-sm-12" style="text-align: center;">
+                <a class="btn btn-md btn-danger" href="#" data-toggle="modal" data-target="#deleteModal">Delete Account</a>
+            </div>
+        </div>
+
+        <div id="deleteModal" class="modal fade">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button class="close" aria-hidden="true" type="button" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Delete Account</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>WARNING! This action will permanent delete your account including your data.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-default" type="button" data-dismiss="modal">Cancel</button>
+                        <button class="btn btn-danger" type="button">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -23,5 +54,9 @@ export default {
 .container-fluid {
   margin-top: 20px;
 }
-</style>
 
+hr {
+  width: 70%;
+  border-color: #adadad;
+}
+</style>
