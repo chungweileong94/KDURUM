03276eae5cdd665de18a887dea0f7dedf9aeaac1
Chung Wei Leong <leongchungwei@hotmail.com>
2018-04-18 19:36:37 +0800

    Updated cache id

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
             {from: "static/manifest.json", to: "manifest.json"}
         ]),
         new SWPrecacheWebpackPlugin({
-            cacheId: "kdurum-app-v3",
+            cacheId: "kdurum-app-v4",
             filename: "service-worker.js",
             staticFileGlobs: ["dist/**.*", "dist/Images/**.*"],
             minify: true,
